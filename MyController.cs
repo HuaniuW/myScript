746907404e6303c7f4f54820b8601121b379dc8d
HuaniuW <whnlove1999@163.com>
2018-06-21 21:10:47 +0800

    新控制类

---
+++ MyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyController : MonoBehaviour {
+    GameBody _body;
+
+    [Header("水平方向")]
+    public float horizontalDirection;
+
+    //水平方向的按键响应 Input.GetAxis
+    const string HORIZONTAL = "Horizontal";
+    // Use this for initialization
+    void Start () {
+        _body = GetComponent<GameBody>();
+        //print("body: "+_body.);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("jump");
+            _body.getJump();
+        }
+
+        horizontalDirection = Input.GetAxis(HORIZONTAL);
+        if (horizontalDirection > 0) {
+            _body.runRight(horizontalDirection);
+        } else if(horizontalDirection<0) {
+            _body.runLeft(horizontalDirection);
+        }
+        else
+        {
+            _body.reSetLR();
+        }
+        
+       
+    }
+}
