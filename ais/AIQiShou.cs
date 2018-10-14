eb351e76264cd8be9bb3acb7c4ca91b521330d4b
HuaniuW <whnlove1999@163.com>
2018-10-15 06:35:49 +0800

    加了起手

---
+++ ais/AIQiShou.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIQiShou : MonoBehaviour {
+
+
+    public bool isQishouAtk = false;
+    public string[] qishouAtkArr = { "backUp","atk_1" };
+    public bool isFirstAtked = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
