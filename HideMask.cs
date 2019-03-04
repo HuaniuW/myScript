26eefb701216351a50e7c89b2828b3fc32d96a48
HuaniuW <whnlove1999@163.com>
2019-03-05 02:56:31 +0800

    0

---
+++ HideMask.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMask : MonoBehaviour {
+    public Transform hideObj;
+	// Use this for initialization
+	void Start () {
+        Color t = this.GetComponent<SpriteRenderer>().color;
+
+        this.GetComponent<SpriteRenderer>().color = new Color(t.r, t.g, t.b, 0.2f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D Coll)
+    {
+        if (Coll.tag == "Player") {
+            //t.gameObject.SetActive(false);
+            hideObj.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D Coll)
+    {
+        //print("Trigger - B");
+        if (Coll.tag == "Player")
+        {
+            hideObj.gameObject.SetActive(true);
+        }
+    }
+    void OnTriggerStay2D(Collider2D Coll)
+    {
+        //print("Trigger - C");
+
+    }
+}
