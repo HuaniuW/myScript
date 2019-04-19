7714bba07f8df981089403dd6e169727c835935b
huaniuw <whn6321475@163.com>
2019-04-19 23:21:09 +0800

    变

--- ais/aiAtkTheWay/AITheWay_dcr.cs
+++ ais/aiAtkTheWay/AITheWay_dcr.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 using System;
 
 public class AITheWay_dcr : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    float theLive;
+    [Header("变招血量下限1")]
+    public float bz1 = 0.5f;
+    [Header("变招血量下限2")]
+    public float bz2 = 0f;
+    // Use this for initialization
+    void Start () {
+        theLive = this.GetComponent<RoleDate>().live;
+    }
 	
+    
 
     public string[] zsArr1 = { "atk_1", "atk_2", "atk_3" };
     public string[] zsArr2 = { "atk_1", "atk_2", "atk_3" };
     public string[] zsArr3 = { "atk_1", "atk_2", "atk_3" };
+    public string[] zsArr4 = { "atk_1", "atk_1", "atk_1" };
+    public string[] zsArr5 = { "atk_1", "atk_1", "atk_1" };
+    public string[] zsArr6 = { "atk_1", "atk_1", "atk_1" };
 
     Array[] atkArrs = new Array[] { };
 
 
     public string[] GetZSArrays(int lie = 0)
     {
-        Array[] atkArrs = { zsArr1, zsArr2, zsArr3 };
+        if (this.GetComponent<RoleDate>().live <= theLive * bz2) {
+
+        }else if(this.GetComponent<RoleDate>().live<= theLive * bz1)
+        {
+            atkArrs = new Array[] { zsArr4, zsArr5, zsArr6 };
+        }
+        else
+        {
+            atkArrs = new Array[] { zsArr1, zsArr2, zsArr3 };
+        }
+        
         //print("a: "+ atkArrs.Length);
         string[] zss = (string[])atkArrs[lie];
         //print(zss[0]);
